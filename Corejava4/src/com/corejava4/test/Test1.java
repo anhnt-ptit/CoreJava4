98eb71228c1f5c8ee7a09c022dc870f77843f490
anhnt <anhnt@DESKTOP-N5IUTJO>
2016-10-27 18:31:06 +0700

    commit after mergece

--- Corejava4/src/com/corejava4/test/Test1.java
+++ Corejava4/src/com/corejava4/test/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Core java 4");
+		System.out.println("Core java 4 frer");
 		System.out.println("Core java 4 333");
 		
 	}
