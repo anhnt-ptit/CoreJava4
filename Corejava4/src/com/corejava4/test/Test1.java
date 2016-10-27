2ac3e776df6e250910f5940f64e1c280ec2e75a1
anhnt-ptit <anhnt.ptit@gmail.com>
2016-10-27 18:22:04 +0700

    test for update
    test for update

--- Corejava4/src/com/corejava4/test/Test1.java
+++ Corejava4/src/com/corejava4/test/Test1.java
@@ -5,7 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Core java 4");
-		System.out.println("Core java 4");
+		System.out.println("Core java 4 333");
+		
 	}
 
 }
